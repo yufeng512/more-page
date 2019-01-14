8fcfc8fbee377eadfa87029d397c08cc2dec022e
xiaoyu <yu.xiaosh@downtown8.com>
2019-01-14 22:53:25 +0800

    video

--- src/components/goods/index.vue
+++ src/components/goods/index.vue
@@ -1,13 +1,81 @@
 <template>
-  <div @click="go">goods</div>
+  <div>
+    <div class="video-box">
+      <video-player  class="video-player vjs-custom-skin"
+        ref="videoPlayer"
+        :playsinline="true"
+        :options="playerOptions"
+        @play="onPlayerPlay($event)"
+        @pause="onPlayerPause($event)"
+        @playing="onPlayerPlaying($event)"
+        @statechanged="playerStateChanged($event)"
+        @ready="playerReadied"
+      ></video-player>
+    </div>
+  </div>
 </template>
 <script>
 export default {
+  data () {
+    return {
+      playerOptions : {
+        playbackRates: [0.7, 1.0, 1.5, 2.0], //播放速度
+        autoplay: false, //如果true,浏览器准备好时开始回放。
+        muted: false, // 默认情况下将会消除任何音频。
+        loop: false, // 导致视频一结束就重新开始。
+        preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+        language: 'zh-CN',
+        aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+        fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+        sources: [{
+          type: "",
+          src: '../../static/111.mp4' //url地址
+        }],
+        // poster: "../../static/images/test.jpg", //你的封面地址
+        // width: document.documentElement.clientWidth,
+        notSupportedMessage: '此视频暂无法播放，请稍后再试', //允许覆盖Video.js无法播放媒体源时显示的默认信息。
+        controlBar: {
+          timeDivider: true,
+          durationDisplay: true,
+          remainingTimeDisplay: false,
+          fullscreenToggle: true  //全屏按钮
+        }
+      }
+    }
+  },
   methods: {
-    go () {
-      this.$router.push('goodsDetail')
+    onPlayerPlay ($event) {
+      let self = this
+      setInterval(()=>{
+        self.onPlayerPlaying()
+      },3000)
+    },
+    onPlayerPause(player) {
+      // console.log('player pause!', player)
+    },
+    onPlayerPlaying(player) {
+      // console.log('player pause!', player)
+    },
+    // ...player event
+
+    // or listen state event
+    playerStateChanged(playerCurrentState) {
+      // console.log('player current update state', playerCurrentState)
+    },
+
+    // player is ready
+    playerReadied(player) {
+      console.log('the player is readied', player)
+      // you can use it to do something...
+      // player.[methods]
     }
   }
 }
 </script>
+<style>
+.video-box{
+  padding: 10px;
+}
+</style>
+
 
--- src/pages/goods/goods.js
+++ src/pages/goods/goods.js
@@ -1,8 +1,12 @@
 import Vue from 'Vue'
+import VideoPlayer from 'vue-video-player'
 import VueRouter from 'vue-router'
 import Goods from './goods.vue'
+import '@/styles/index.sass'
 import routes from '../../router/goods.js'
-console.log(routes)
+import 'video.js/dist/video-js.css'
+import 'vue-video-player/src/custom-theme.css'
+Vue.use(VideoPlayer)
 
 Vue.use(VueRouter)
 var router = new VueRouter({
