7058a21ddf1a22f131ba9ef3d2932e2a3db8d7b8
York <York.xiao@arvato.com>
2019-02-20 13:20:04 +0800

    门店导航

--- src/components/storeSearch/tpl.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div id='redituser'>
-        <h4 style='font-size:14px'>{{title}}</h4>
-        <p style='margin:6px 0;font-size:12px;color:#666666;'>{{address}}</p>
-        <button @click='goDetails' style='color:#666666;font-size:12px;'>到这里去</button>
-    </div>
-</template>
