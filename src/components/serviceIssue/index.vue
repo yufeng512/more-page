249aed4b7f5e4106258da7a3687bbe3bcee01e76
xiaoyu <yu.xiaosh@downtown8.com>
2019-03-29 17:19:32 +0800

    bug

--- src/components/serviceIssue/index.vue
+++ src/components/serviceIssue/index.vue
@@ -1,10 +1,15 @@
 <template>
     <div class="issue-box">
+      <div class="bg">
+        <img src="@/assets/quest_bg.png" alt="">
+      </div>
+      <div class="container">
         <img class="logo-box" src="@/assets/logo_wx.png" alt="">
         <p>欧舒丹会员调查评价</p>
         <div class="btn-box">
           <el-button type="primary" @click="submit">点击进入</el-button>
         </div>
+      </div>
     </div>
 </template>
 <script>
@@ -29,32 +34,39 @@ export default {
   margin: 15px 0
   display: block
 .issue-box
-  height: 100%
-  background-size: cover
-  background-repeat: no
-  background-image: url(../../assets//quest_bg.png)
-  p
-    padding-top: 12.25rem
-    font-size: 2rem
+  position: relative
+  .bg
+    position: absolute
+    top: 0
+    z-index: 0
+    img
+      width: 100%
+  .container
+    position: absolute
     width: 100%
-    font-weight: 300
-    text-align: center
-    color: #f8bc32
-    line-height: 1.2rem
-    padding-bottom: 0.675rem
-    background: url(../../assets/border_quest.png) no-repeat bottom
-    background-size: 13.325rem 1px
-  .logo-box
-    width: 50%
-    margin-left: 25%
-    padding-top: 2.9rem
-  .btn-box 
-    margin-top: 5.1rem
-    text-align: center
-    button
-      width: 72%
-      border: none
-      background: #f8bc32
+    z-index: 1
+    p
+      padding-top: 12.25rem
+      font-size: 2rem
+      width: 100%
+      font-weight: 300
+      text-align: center
+      color: #f8bc32
+      line-height: 1.2rem
+      padding-bottom: 0.675rem
+      background: url(../../assets/border_quest.png) no-repeat bottom
+      background-size: 13.325rem 1px
+    .logo-box
+      width: 50%
+      margin-left: 25%
+      padding-top: 2.9rem
+    .btn-box 
+      margin-top: 5.1rem
+      text-align: center
+      button
+        width: 72%
+        border: none
+        background: #f8bc32
 
 </style>
 
