c6692898754b36e4533f09f8bb8e0aac75146855
xiaoyu <yu.xiaosh@downtown8.com>
2019-03-29 21:23:06 +0800

    卡券

--- src/components/card/index.vue
+++ src/components/card/index.vue
@@ -107,6 +107,7 @@ export default {
         var num=str.indexOf("?")
         str=str.substr(num+1); //取得所有参数   stringvar.substr(start [, length ]
         var arr=str.split("&"); //各个参数放到数组里
+        alert('res'+JSON.stringify(arr))
         for(var i=0;i < arr.length;i++){
           num=arr[i].split("=");
           if(num[0]=='encrypt_code'){
