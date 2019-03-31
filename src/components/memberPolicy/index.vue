68c398f3ea92a7415ad99800a434271aa362fdf2
xiaoyu <yu.xiaosh@downtown8.com>
2019-03-31 21:22:20 +0800

    手机绑定

--- src/components/memberPolicy/index.vue
+++ src/components/memberPolicy/index.vue
@@ -248,7 +248,7 @@ export default {
       params.openId = localStorage.getItem("openId")
       params.unionId = localStorage.getItem("unionId")
       MemberCheckBind(params).then(res=>{
-        // alert(JSON.stringify(res))
+        alert(JSON.stringify(res))
         if(res.code == 0){
           self.$toast(res.msg||'已绑定成功');
           setTimeout(function(){
