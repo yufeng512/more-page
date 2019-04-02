0861a9d9590febee2aab692c261221b791fed439
xiaoyu <yu.xiaosh@downtown8.com>
2019-04-02 18:38:53 +0800

    获取当前位置

--- src/api/common.js
+++ src/api/common.js
@@ -10,7 +10,7 @@ export function addQuestion(params) {
   return post(api.addQuestion, params)
 }
 export function getlocalAPi(params) {
-  return post(api.localAPi, params)
+  return post(api.localAPi + '?originId=' + params.originId + '&url=' + params.url)
 }
 export function hasPartQues(req) {
   return fetch(api.hasPartQues + req.openId + '/' + req.campaignId)
