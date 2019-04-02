eabb78c0103068aaa42e5ec99dd4378dd80ccbe0
xiaoyu <yu.xiaosh@downtown8.com>
2019-04-02 19:56:53 +0800

    获取当前位置

--- src/components/storeSearch/index.vue
+++ src/components/storeSearch/index.vue
@@ -87,9 +87,8 @@ export default {
                     originId:'gh_4b16ea1dcc6f',
                     url: 'https://crm.eloccitane.com/wmth5/storeSearch.html'
             }).then(res=>{
-                alert('res222'+JSON.stringify(res))
                 wx.config({
-                    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                     appId: res.appId, // 必填，公众号的唯一标识
                     timestamp: res.timestamp, // 必填，生成签名的时间戳
                     nonceStr: res.nonceStr, // 必填，生成签名的随机串
@@ -101,11 +100,12 @@ export default {
                     wx.getLocation({
                         type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
                         success: function (r) {
+                            // alert('res222'+JSON.stringify(r))
                             self.latitude = r.latitude; // 纬度，浮点数，范围为90 ~ -90
                             self.longitude = r.longitude; // 经度，浮点数，范围为180 ~ -180。
-                            var mk = new BMap.Marker({lat:self.latitude,lng:self.longitude});
-                                map.addOverlay(mk);
-                                map.panTo({lat:self.latitude,lng:self.longitude});
+                            let option = { lat: self.latitude, lng: self.longitude }
+                            let point = new BMap.Point(option.lng, option.lat)
+                            let marker = new BMap.Marker(point);
                             let params = {
                                 longitude: self.longitude,
                                 latitude: self.latitude
