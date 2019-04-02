6fffeb3857ea38ac50ce7e7b743a3325096f62e0
xiaoyu <yu.xiaosh@downtown8.com>
2019-04-02 16:58:54 +0800

    获取当前位置

--- src/components/storeSearch/index.vue
+++ src/components/storeSearch/index.vue
@@ -100,7 +100,7 @@ export default {
                 }
             },{enableHighAccuracy: false})
             GetCardSign(localStorage.getItem('memberCode')).then(res=>{
-                // alert('res'+JSON.stringify(res))
+                alert('res'+JSON.stringify(res))
                 wx.config({
                     debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                     appId: 'wx60671049a2f3e0f4', // 必填，公众号的唯一标识
