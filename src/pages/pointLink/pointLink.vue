5ff209ef4afce3c40c372119e34b2a3d6a685ae3
xiaoyu <yu.xiaosh@downtown8.com>
2019-04-09 15:50:46 +0800

    link

---
+++ src/pages/pointLink/pointLink.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <loading v-show="$store.getters.showLoading"></loading>
+    <router-view/>
+  </div>
+</template>
+<script>
+
+import loading from '@/components/loading'
+export default {
+  name: 'pointShop',
+  data () {
+    return {}
+  },
+  components: {
+    loading
+  },
+  mounted() {
+  }
+}
+</script>
